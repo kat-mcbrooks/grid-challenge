7dd6d60dfe2389e52a6e74bd226589dd44d4b705
kat <katharinebrooks@hotmail.co.uk>
2022-04-20 14:59:28 +0100

    first test case with 5 seconds passing

--- test_grid_challenge.py
+++ test_grid_challenge.py
@@ -74,3 +74,61 @@ def test_bomberPlane_3seconds_multiple_rows():
         "...OOOO",
         "...OOOO",
     ]
+
+
+#       "OOO4OOO",
+#       "OO444OO",
+#       "OOO444O",
+#       "44OO4OO",
+#       "444OOOO",
+#       "444OOOO",
+# after 5:
+#       ".......",
+#       "...4...",
+#       "....4..",
+#       "....4..",
+#       "44.....",
+#       "44.....",
+
+
+def test_bomberPlane_3seconds_multiple_rows_pre_5seconds():
+    assert bomberPlane(3, [".......", "...O.O.", "....O..", "..O....", "OO...OO", "OO.O..."]) == [
+        "OOO.O.O",
+        "OO.....",
+        "OO....O",
+        ".......",
+        ".......",
+        ".......",
+    ]
+
+
+def test_bomberPlane_5seconds_multiple_rows_A():
+    assert bomberPlane(5, [".......", "...O.O.", "....O..", "..O....", "OO...OO", "OO.O..."]) == [
+        ".......",
+        "...O.O.",
+        "...OO..",
+        "..OOOO.",
+        "OOOOOOO",
+        "OOOOOOO",
+    ]
+
+
+# after 4 secs:
+#       "OOO4O4O",
+#       "OO44444",
+#       "OO4444O",
+#       "4444444",
+#       "4444444",
+#       "4444444",
+# after 5 secs:
+
+
+# def test_bomberPlane_5seconds_multiple_rows():
+#     assert bomberPlane(5, [".......", "...O...", "....O..", ".......", "OO.....", "OO....."]) == [
+#         "OOO.OOO",
+#         "OO...OO",
+#         "OOO...O",
+#         "..OO.OO",
+#         "...OOOO",
+#         "...OOOO",
+#     ]
