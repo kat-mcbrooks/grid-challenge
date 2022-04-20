d0d05936f362adbe4b77c29f6bcf942e2bfeb10f
kat <katharinebrooks@hotmail.co.uk>
2022-04-20 13:20:12 +0100

    can also handle 2 seconds with one column, 2 rows

--- test_grid_challenge.py
+++ test_grid_challenge.py
@@ -5,6 +5,10 @@ def test_bomberPlane_2seconds_1_row():
     assert bomberPlane(2, ["O..OO.O"]) == ["OOOOOOO"]
 
 
+def test_bomberPlane_2seconds_2_row_1_col():
+    assert bomberPlane(2, ["O", "."]) == ["O", "O"]
+
+
 def test_bomberPlane_2seconds_multiple_rows():
     assert bomberPlane(2, ["OOO.OOO", "OO...OO", "OOO...O", "..OO.OO", "...OOOO", "...OOOO"]) == [
         "OOOOOOO",
