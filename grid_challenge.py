74e9c4b7b2402279d253ab728cb22d5eeb0d31a4
kat <katharinebrooks@hotmail.co.uk>
2022-04-20 13:18:39 +0100

    passing 2 second test, with multiple rows in the grid

--- grid_challenge.py
+++ grid_challenge.py
@@ -1,6 +1,10 @@
 import itertools
 
 
+def join(row):
+    return "".join(row)
+
+
 def bomberPlane(n, grid):
     r = len(grid)  # number of rows in grid. 6 in our example
     c = len(grid[0])  # number of characters rather than length of list. 7 in our example
@@ -8,6 +12,12 @@ def bomberPlane(n, grid):
     #
     if n % 2 == 0:  # every 2 seconds, bombs are added to every cell i.e. all zeros
         # create a list of sublists of zeros, with the same number of rows and columns as the initial grid
-        zeros_grid = ["O" for i in range(c) for x in itertools.repeat(None, r)]
-        print(zeros_grid)
+        zeros_grid = [["O" for i in range(c)] for x in itertools.repeat(None, r)]
+        # print(zeros_grid)
+
+        modifiedList = list(map(join, zeros_grid))
+        print(modifiedList)
+        return modifiedList
+        # for row in zeros_grid:
+        #     zeros_grid_of_strings.append("".join(sublist))
         return ["".join(zeros_grid)]
--- test_grid_challenge.py
+++ test_grid_challenge.py
@@ -3,3 +3,14 @@ from grid_challenge import bomberPlane
 
 def test_bomberPlane_2seconds_1_row():
     assert bomberPlane(2, ["O..OO.O"]) == ["OOOOOOO"]
+
+
+def test_bomberPlane_2seconds_multiple_rows():
+    assert bomberPlane(2, ["OOO.OOO", "OO...OO", "OOO...O", "..OO.OO", "...OOOO", "...OOOO"]) == [
+        "OOOOOOO",
+        "OOOOOOO",
+        "OOOOOOO",
+        "OOOOOOO",
+        "OOOOOOO",
+        "OOOOOOO",
+    ]
